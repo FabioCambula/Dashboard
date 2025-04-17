dde34bf148a40f1bec71fae6f4b1c1bfeff40bc2
FabioCambula <fabiocambulaa@yahoo.com>
2025-04-17 11:48:15 +0200

    update index js

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,14 @@ import WeatherBox from "../components/WeatherBox.vue";
 import LogBox from "../components/LogBox.vue";
 
 const routes = [
-  { path: "Dashboard/", redirect: "Dashboard/solar" },
-  { path: "Dashboard/solar", component: EnergyBox },
-  { path: "Dashboard/weather", component: WeatherBox },
-  { path: "Dashboard/logs", component: LogBox },
+  { path: "/", redirect: "/solar" },
+  { path: "/solar", component: EnergyBox },
+  { path: "/weather", component: WeatherBox },
+  { path: "/logs", component: LogBox },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('Dashboard/'),
   routes,
 });
 
